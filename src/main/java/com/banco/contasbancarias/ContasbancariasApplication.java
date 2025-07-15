a0399ddee90cea646dd96876f8a6e7afa8aa357c
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-07-15 20:30:48 -0300

    Projeto base

---
+++ src/main/java/com/banco/contasbancarias/ContasbancariasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.contasbancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasbancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasbancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/contasbancarias/ContasbancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.contasbancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasbancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
